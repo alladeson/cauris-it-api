8b8504e1ccd404807a03909ac930bafb1b3c36cc
alladeson <alladeson91@gmail.com>
2022-04-28 17:50:00 +0100

    mise à jour de l'entité taxe

--- src/main/java/com/alladeson/caurisit/models/entities/Taxe.java
+++ src/main/java/com/alladeson/caurisit/models/entities/Taxe.java
@@ -14,72 +14,82 @@ import javax.persistence.Id;
  */
 @Entity
 public class Taxe extends BaseEntity {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2014954712004806447L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	private Long id;
 	private TaxeGroups groupe;
 	private String libelle;
 	private Integer valeur;
+
 	/**
 	 * @return the id
 	 */
 	public Long getId() {
 		return id;
 	}
+
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the groupe
 	 */
 	public TaxeGroups getGroupe() {
 		return groupe;
 	}
+
 	/**
 	 * @param groupe the groupe to set
 	 */
 	public void setGroupe(TaxeGroups groupe) {
 		this.groupe = groupe;
 	}
+
 	/**
 	 * @return the libelle
 	 */
 	public String getLibelle() {
 		return libelle;
 	}
+
 	/**
 	 * @param libelle the libelle to set
 	 */
 	public void setLibelle(String libelle) {
 		this.libelle = libelle;
 	}
+
 	/**
 	 * @return the valeur
 	 */
 	public Integer getValeur() {
 		return valeur;
 	}
+
 	/**
 	 * @param valeur the valeur to set
 	 */
 	public void setValeur(Integer valeur) {
 		this.valeur = valeur;
 	}
+
 	/**
 	 * @return the string of object
 	 */
 	public String getString() {
-		return this.groupe + " - " + this.libelle + " (" +  this.valeur + "%)";
+		return this.groupe + " (" + this.valeur + "%) - " + this.libelle;
 	}
+
 	/**
 	 * @return the serialversionuid
 	 */
